02dc0cb289dde7e980e25b5fdfa9497ace6b02af
Shinho Lee <cytron1225@naver.com>
2026-01-21 20:56:17 +0900

    login joing /api

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
-const BASE_URL = "http://3.34.179.129:8080";
+const BASE_URL = "/api";
 
 // --- API 로직 ---
 async function checkEmailExistsFetch(email: string): Promise<boolean> {
